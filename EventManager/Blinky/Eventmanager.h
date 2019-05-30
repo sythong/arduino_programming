a04960fe7d555f15d11fdc8b7545b3539ae7ceef
sythong <sythongmta@gmail.com>
2019-05-30 09:31:30 +0200

    add new event manager

---
+++ EventManager/Blinky/Eventmanager.h
@@ -0,0 +1,19 @@
+#ifndef EVENMANAGER_H
+#define EVENMANAGER_H
+
+#include <limits.h>
+#include <Arduino.h>
+
+
+#define EVENTUALLY_MAX_CONTEXTS 10
+#define EVENTUALLY_MAX_LISTENERS 20
+
+class EvtManager;   // Quan ly  su kien
+class EvtContext;   // Noi dung cua Su kien
+class EvtListener;  // Lang nghe Su kien (Duoc kich hoat or not)
+
+
+
+
+
+#endif
